2335fc60d93b0e8925d14650470c166403f8df34
JoãoGabs <85592186+Ja1Gabs@users.noreply.github.com>
2023-09-06 16:31:25 -0300

    Add files via upload

---
+++ Atividade 5/Atividade5.js
@@ -0,0 +1,18 @@
+
+x = 1;
+var y = 2;
+
+function exemplo() {
+
+    var z = 3; 
+    w = 4;
+     let i=5
+     
+     x = x * 10;
+     y = y * 10;
+    return 'x-${x} y=${y} z=${z} w=${w} i=${i}';
+}
+
+alert(exemplo());
+
+
