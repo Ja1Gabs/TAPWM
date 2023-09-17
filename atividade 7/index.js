98ca4991bd468404a8e9a8f50cfcf2a9d4ac8be5
JoãoGabs <85592186+Ja1Gabs@users.noreply.github.com>
2023-09-17 14:24:44 -0300

    Update index.js

--- atividade 7/index.js
+++ atividade 7/index.js
@@ -1,34 +1,18 @@
-var pedra = 1
-var papel = 2
-var tesoura = 3
+var pedra = 1;
+var papel = 2;
+var tesoura = 3;
 
+var escolha = parseInt(prompt('Pedra = 1, Papel = 2 ou Tesoura = 3'));
+var escolhaAle = Math.floor(Math.random() * 3) + 1;
 
-
-
-
-
-
-   var escolha = prompt('pedra = 1, papel = 2 ou tesoura = 3')
-   var escolhaAle = Math.floor((Math.random() * 3) + 1);
-  
-
-if (escolha == 1 && escolhaAle == 3)
- alert("pedra quebra tesoura")
- if (escolha == 3 && escolhaAle == 1)
- alert("pedra quebra tesoura")
- if (escolha == 2 && escolhaAle == 3)
-alert("tesoura corta papel")
- if (escolha == 3 && escolhaAle == 2)
-alert("tesoura corta papel")
- if (escolha == 2 && escolhaAle == 1)
-alert("papel cobre pedra")
-if (escolha == 1 && escolhaAle == 2)
-alert("papel cobre pedra")
- if(escolha == 1 && escolhaAle == 1)
-alert("Empate")
- if(escolha == 2 && escolhaAle == 2)
-alert("Empate")
- if(escolha == 3 && escolhaAle == 3)
-alert("Empate")
-
-
+if (escolha === 1 && escolhaAle === 3 || escolha === 3 && escolhaAle === 1) {
+  alert("Pedra quebra Tesoura");
+} else if (escolha === 2 && escolhaAle === 3 || escolha === 3 && escolhaAle === 2) {
+  alert("Tesoura corta Papel");
+} else if (escolha === 2 && escolhaAle === 1 || escolha === 1 && escolhaAle === 2) {
+  alert("Papel cobre Pedra");
+} else if (escolha === escolhaAle) {
+  alert("Empate");
+} else {
+  alert("Escolha inválida! Por favor, escolha entre 1, 2 ou 3.");
+}
